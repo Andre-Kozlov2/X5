4bb611f76fefc84c0fad00c12e177b72fe60fa43
Андрей Козлов <465@oktell.ru>
2020-09-14 22:20:47 +0300

    Refactor filename

--- src/second/lesson/task_07.java
+++ src/second/lesson/task_07.java
@@ -9,23 +9,25 @@ public class task_07 {
         int value = scanner.nextInt();
         double n;
         switch (value) {
-            case 905 -> {
+            case 905:
                 n = 10 * 4.15;
                 System.out.println("Москва. Стоимость разговора: " + n);
-            }
-            case 194 -> {
+                break;
+            case 194:
                 n = 10 * 1.98;
                 System.out.println("Ростов. Стоимость разговора: " + n);
-            }
-            case 491 -> {
+                break;
+            case 491:
                 n = 10 * 2.69;
                 System.out.println("Краснодар. Стоимость разговора: " + n);
-            }
-            case 800 -> {
+                break;
+            case 800:
                 n = 10 * 5;
                 System.out.println("Киров. Стоимость разговора: " + n);
-            }
-            default -> System.out.println("Код города не определен по условию задачи");
+                break;
+            default:
+                System.out.println("Код города не определен по условию задачи");
+                break;
         }
     }
 }
